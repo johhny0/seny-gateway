c01239610ae3e70da25f2d6de450609dec8469b8
Johhny <jhasse20@hotmail.com>
2022-12-09 23:10:21 -0300

    the express part

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import express, { Router, Request, Response } from "express"
+
+const app = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.all("/", (req: Request, res: Response) => {
+    res.json({ msg: "sausifufu" })
+})
+
+app.use(route);
+
+app.listen(3333, () => "Server running!");
+
