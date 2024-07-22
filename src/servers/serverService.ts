932ba07e0079a284a67ea89dbf330270c1d9d2be
Johhny <jhasse20@hotmail.com>
2024-07-22 00:29:49 -0300

    add type for requests, changed service name, change url for docker connection

--- src/servers/serverService.ts
+++ src/servers/serverService.ts
@@ -1,16 +1,16 @@
 import serversJson from "../../servers.json";
 import { Server } from "./server";
 import httpProxy from "express-http-proxy";
-import { Express } from "express";
+import { Express, Response } from "express";
 
 export class ServerService {
     servers: Server[] = serversJson;
 
     loadRoutes(app: Express) {
-        app.get("/", (_, res) => res.json({ msg: "Middleware Runnin! 🌉" }));
+        app.get("/", (_, res: Response) => res.json({ msg: "Middleware Runnin! 🌉" }));
 
         this.servers.forEach((server: Server) => app.use(server.path, httpProxy(server.url)));
 
-        app.all("*", (_, res) => res.status(404).json({ msg: "Middleware: Route Does Not Exists! 🛑🤚" }))
+        app.all("*", (_, res: Response) => res.status(404).json({ msg: "Middleware: Route Does Not Exists! 🛑🤚" }))
     }
 }
